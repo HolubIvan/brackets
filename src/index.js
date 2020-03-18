450330c7a06530ee98bbf2f5a172bf02641357a3
Ivan Holub <ivanholub.js@gmail.com>
2020-03-18 13:43:55 +0200

    feat: resolved test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = bracketsConfig.map(function(el){
+        return el.join('');
+      })
+    
+      for(let i = 0; i < brackets.length; i++){
+        if(str.includes(brackets[i])){
+            str.replace(brackets[i], "")
+            i = -1;
+        }
+      }
+      return (str) ? true : false;
 }
